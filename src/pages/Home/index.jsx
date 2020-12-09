2668b749e35c494b68e06837d8ac84b27aa82826
Carlos Henrique <heinrickcostta@gmail.com>
2020-12-09 07:22:28 -0300

    feate: create src/Home/index.jsx

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,35 @@
+import React,{ useState } from 'react';
+
+import Nav from '../../components/Nav/index'
+
+import ImagnsBooking from '../../assets/imageCalendar.svg'
+
+import ModalPlaces from  '../../components/Modal/Modal_Places/index'
+import ModalContentPlaces from '../../components/Modal/Modal_Places/modal.context'
+
+import './styles.css'
+
+const Home = () => {
+
+    const [ modalOpenPlaces, setModalOpenPlaces] = useState(false)
+
+    return (
+        <>
+        <div>
+            < Nav />
+        </div>
+        <p className="text_main">Explore Reservas</p>
+        <h5>Pesquise centenas de lugares de uma só vez e encontre Restaurantes, Hotéis, Bares para você fazer sua reserva na comodidade de sua casa.</h5>
+        <button className="button_home" onClick={() => setModalOpenPlaces(true)}>Tipos de Reservas</button>
+        <img className="main_image" src={ImagnsBooking} alt="image"/>
+
+
+        <ModalPlaces modalOpen={modalOpenPlaces}>
+            <ModalContentPlaces setModalOpen={ setModalOpenPlaces } />  
+        </ModalPlaces> 
+        </>
+        
+    );
+}
+
+export default Home;
