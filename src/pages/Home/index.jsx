365238a1e6627f4788573e84a834a8e9f80a7830
Henrique <Carlos Henrique>
2021-01-09 08:15:55 -0300

    modified: src/pages/Home/index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         <p className="text_main">Explore Reservas</p>
         <h5>Pesquise centenas de lugares de uma só vez e encontre Restaurantes, Hotéis, Bares para você fazer sua reserva na comodidade de sua casa.</h5>
         <button className="button_home" onClick={() => setModalOpenPlaces(true)}>Tipos de Reservas</button>
-        <img className="main_image" src={ImagnsBooking} alt="image"/>
+        <img className="main_image" src={ImagnsBooking} alt="booking"/>
 
 
         <ModalPlaces modalOpen={modalOpenPlaces}>
