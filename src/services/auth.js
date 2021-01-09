b7665a51455be58cb6459a4d8b453c377f19b495
Henrique <Carlos Henrique>
2021-01-09 08:17:59 -0300

    create: src/services/auth.js

---
+++ src/services/auth.js
@@ -0,0 +1,11 @@
+
+export const TOKEN_KEY = localStorage.getItem('token');
+export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null || undefined;
+export const getToken = () => localStorage.getItem(TOKEN_KEY);
+
+export const login = token => {
+  localStorage.setItem(TOKEN_KEY, token);
+};
+export const logout = () => {
+  localStorage.removeItem(TOKEN_KEY);
+};
